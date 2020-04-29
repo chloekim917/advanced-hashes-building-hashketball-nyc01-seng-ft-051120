9658ec055340e6d31fb1937e80b8271388925de8
Seoyoung Kim <mintholic1@gmail.com>
2020-04-29 18:21:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,16 +4,17 @@ def game_hash
       :team_name => "Brookly Nets",
       :colors => ["Black", "White"],
       :players => [{
-        :player_name => "",
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steal => ,
-        :blocks => ,
-        :slam_dunks =>
-      }]
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steal => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      }, 
+      ]
     },
     :away => { :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
