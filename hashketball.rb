37264e1b89dcfd97fe85f131bd8fecca61db06bf
Seoyoung Kim <mintholic1@gmail.com>
2020-04-29 18:20:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,32 @@
 def game_hash
   basketball = {
     :home => {
-      :team_name => "",
-      :colors => [""],
-      :players => [{}]
+      :team_name => "Brookly Nets",
+      :colors => ["Black", "White"],
+      :players => [{
+        :player_name => "",
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steal => ,
+        :blocks => ,
+        :slam_dunks =>
+      }]
     },
-    :away => {}
+    :away => { :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [{
+        :player_name => "",
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steal => ,
+        :blocks => ,
+        :slam_dunks =>
+      }]
+    }
   }
